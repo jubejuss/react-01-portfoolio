0d6433914424d99213aaf6fe1f8016efb651b03a
Juho Kalberg <jubejuss@gmail.com>
2024-04-03 00:20:18 +0300

    set base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-01-portfoolio/',
+  base: 'https://jubejuss.github.io/react-01-portfoolio/',
 })
